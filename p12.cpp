dbf5f6b4e9e951589e89afd3e8a81c0f320a32df
Vishwa Sudaraka <vishwa25flash@gmail.com>
2024-11-17 20:14:29 +0530

    Add files via upload

---
+++ p12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout<< "Enter the celsius value" <<endl;
+    int c;
+    cin>>c;
+    int F;
+    F=(9/5*c) + 32;
+    cout << "Fahrenheit value is =" << F;
+    cout <<endl;
+    return 0;
+}
+
